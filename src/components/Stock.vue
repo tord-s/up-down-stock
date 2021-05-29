d3ddc6abd952dc4ad06323d8b2f30fcbf880396a
tord-s <tord@softeland.name>
2021-05-29 15:02:06 +0200

    first commit

---
+++ src/components/Stock.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- <router-link :to="{ name: 'Stock', params: { position_id: stock.key } }"> -->
+  <div class="preview_content">
+    <div class="s1">
+      <h3 class="title">{{ stock.open }}</h3>
+      <p class="company">{{ stock.high }}</p>
+    </div>
+    <h1>Hi 2</h1>
+    <div class="s2">
+      <p class="deadline">📅 {{ stock.low }}</p>
+      <p class="location">📍 {{ stock.close }}</p>
+    </div>
+  </div>
+  <!-- </router-link> -->
+</template>
+
+<script>
+export default {
+  name: "Stock",
+  props: {
+    stock: Object,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    // lengthValues() {
+    //     return this.values.length
+    // }
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+
+.preview_content {
+  display: flex;
+  justify-content: space-between;
+  width: 50vw;
+  padding: 10px 30px 10px 30px;
+  margin: 10px;
+  background: #0d698b;
+}
+
+.s1 {
+  flex-grow: 5;
+  text-align: left;
+}
+
+.s2 {
+  flex-grow: 3;
+  text-align: right;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <h1>Hi there!</h1>
+    <AllStocks />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AllStocks from "@/components/AllStocks";
+
+export default {
+  name: "Home",
+  components: {
+    AllStocks,
+  },
+};
+</script>
